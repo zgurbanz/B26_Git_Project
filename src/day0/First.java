90b35bdc8eb7822fe39f088b9de84b410882d9ff
zgurbanz <98862873+zgurbanz@users.noreply.github.com>
2022-03-26 16:07:18 -0400

    updaeted with three comments

---
+++ src/day0/First.java
@@ -0,0 +1,16 @@
+package day0;
+
+public class First {
+    public static void main(String[] args) {
+
+        //TODO: Works for tomorrow
+        // add some tools
+
+        // extra line one
+        // extra line two
+        // extra line three
+
+
+
+    }
+}
