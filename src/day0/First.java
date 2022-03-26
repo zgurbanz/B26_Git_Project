f893dcbe8a529d7e59e9c6cc12467b64a8e1a24a
zgurbanz <98862873+zgurbanz@users.noreply.github.com>
2022-03-26 16:10:32 -0400

    updaeted with three comments

--- src/day0/First.java
+++ src/day0/First.java
@@ -3,8 +3,8 @@ package day0;
 public class First {
     public static void main(String[] args) {
 
-        //TODO: Works for tomorrow
-        // add some tools
+        System.out.println("I like Java");
+        System.out.println("We learn Java for fun...");
 
         // extra line one
         // extra line two
